a4c2e75a26f8407f0746c5011fd378e8e138a933
cgrimm <cgrimm@trinity.edu>
2026-04-18 00:07:59 -0500

    expanded ignore

--- my-app/.next/dev/static/chunks/src_app_page_tsx_1cf6b850._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_4679fe37._.js",
-  "static/chunks/src_app_81c506cb._.css"
-],
-    source: "dynamic"
-});
